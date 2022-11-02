ed18c3072050bb0a0d59fbefb54a1013b50d858b
Jessie Whiting <jessienwhiting@gmail.com>
2022-11-01 20:20:31 -0400

    Add project files.

---
+++ DeckOfCardsAPI/Models/Deck.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+    public class Deck
+    {
+        public bool success { get; set; }
+        public string deck_id { get; set; }
+        public int remaining { get; set; }
+        public bool shuffled { get; set; }
+    }
---
+++ DeckOfCardsAPI/Models/DeckOfCardsDAL.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+
+namespace DeckOfCardsAPI.Models
+{
+    public class DeckOfCardsDAL 
+    {
+        public Deck GetDeck(int num)
+        {
+            string url = $@"https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1{num}";
+            var client = new RestClient(url);
+            var request = new RestRequest();
+            var response = client.GetAsync<Deck>(request);
+            Deck d = response.Result;
+            return d;
+        }
+
+        public DeckHand DeckResults(string id)
+        {
+            string url = $@"https://deckofcardsapi.com/api/deck/{id}/draw/?count=5";
+            var client = new RestClient(url);
+            var request = new RestRequest();
+            var response = client.GetAsync<DeckHand>(request);
+            DeckHand dh = response.Result;
+            return dh;
+        }
+
+
+
+    }
+}
